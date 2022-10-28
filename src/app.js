657600ba8d60cc94377b16580dbb1298e3c1f9f6
Mahe077 <2019cs077@stu.ucsc.lk>
2022-10-28 16:10:04 +0530

    Mobile payments service

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const mobileRouter = require('./routers/mobile')
-var cors = require('cors')
+const express = require("express");
+const mobileRouter = require("./routers/mobile");
+var cors = require("cors");
 
-const port = process.env.PORT
+require("./db/mongoose");
 
-const app = express()
+const port = process.env.PORT;
 
-app.use(cors())
-app.use(express.json())
-app.use(mobileRouter)
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(mobileRouter);
 
 app.listen(port, () => {
-    console.log('server listening on port ' + port)
-})
+  console.log("server listening on port " + port);
+});
--- src/routers/mobile.js
+++ src/routers/mobile.js
@@ -1,13 +1,15 @@
 const express = require("express");
 
+const Auth = require("../middleware/auth");
+
 const router = new express.Router();
 
 //signup
-router.post("/mobile", async (req, res) => {
+router.post("/mobile", Auth, async (req, res) => {
   try {
-    res.status(201).send({ msg:"Your payment is successfully added to the your mobile bill" });
+    res.status(201).send({ status: "success" });
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send({ status: "failed" });
   }
 });
 
